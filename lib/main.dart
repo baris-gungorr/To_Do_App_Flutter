82bd43b89a216caedbe0621e9c093af2226600e9
Baris Gungor <gungorbaris41@gmail.com>
2023-11-15 23:37:37 +0300

    added navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:todoapp/constans/color.dart';
+import 'package:todoapp/screenn/home.dart';
 import 'package:todoapp/todoitem.dart';
 
 void main() {
@@ -14,104 +15,14 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-List<String> todo = ["Study Lessons", "Run 5K", "Go to party"];
-List<String> completed = ["Game meetup", "Take out", "Take out tash"];
-
 class _MyAppState extends State<MyApp> {
+  bool icChecked = false;
+
   @override
   Widget build(BuildContext context) {
-    double deviceHeight = MediaQuery.of(context).size.height;
-    double deviceWidth = MediaQuery.of(context).size.width;
-
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: HexColor(backgroundColor),
-          body: Column(
-            children: [
-              // Header
-              Container(
-                width: deviceWidth,
-                height: deviceHeight / 3,
-                decoration: const BoxDecoration(
-                  color: Colors.purple,
-                  image: DecorationImage(
-                    image: AssetImage("lib/assets/images/header.png"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: const Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 20),
-                      child: Text(
-                        "October 20, 2022",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 40),
-                      child: Text(
-                        "My Todo List",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // Top Column
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: SingleChildScrollView(
-                      child: ListView.builder(
-                    primary: false,
-                    shrinkWrap: true,
-                    itemCount: todo.length,
-                    itemBuilder: (context, index) {
-                      return TodoItem(title: todo[index]);
-                    },
-                  )),
-                ),
-              ),
-              // Complete Text
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Completed",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                ),
-              ),
-              // Bottom Column
-              Expanded(
-                child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: ListView.builder(
-                        primary: false,
-                        shrinkWrap: true,
-                        itemCount: completed.length,
-                        itemBuilder: (context, index) {
-                          return TodoItem(title: completed[index]);
-                        })),
-              ),
-              ElevatedButton(
-                  onPressed: () {}, child: const Text("Add New Task"))
-            ],
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
